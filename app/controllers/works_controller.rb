0f5178634d23e952bbc74e9e651ecb04705b6886
Mathieu Heynen <contact.heynen@gmail.com>
2025-05-28 16:15:53 +0200

    working on the show page and the next previous button

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,7 @@
 class WorksController < ApplicationController
   def show
     @work = Work.find_by!(slug: params[:slug])
+    @previous_work = Work.where("id < ?", @work.id).order(id: :desc).first
+    @next_work = Work.where("id > ?", @work.id).order(id: :asc).first
   end
 end
