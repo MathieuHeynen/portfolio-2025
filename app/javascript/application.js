8a9c5a23636c54e8911579c050ff76ea9558224e
Mathieu Heynen <contact.heynen@gmail.com>
2025-11-24 13:49:24 +0100

    Fix importmap and remove manual script tags fourth attempt

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "./header_height";
-import "./navbar_animation";
-import "./banner_animation";
-import "./scroll_to";
-import "./parallax_effect";
-import "./burger.js";
-import "./carousel_controller.js";
-import "./roulette_effect.js";
-import "./roulette_footer_backTop.js";
+import "custom/burger";
+import "custom/roulette_effect";
+import "custom/header_height";
+import "custom/navbar_animation";
+import "custom/parallax_effect";
+import "custom/carousel_controller";
+import "custom/roulette_footer_backTop";
+import "custom/scroll_to";
+import "custom/banner_animation";
