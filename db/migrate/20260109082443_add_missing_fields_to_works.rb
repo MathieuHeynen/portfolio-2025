4501199740fd789c14b3be5104f6decd915d2b3f
Mathieu Heynen <contact.heynen@gmail.com>
2026-01-23 13:00:21 +0100

    Remove duplicate migration

--- db/migrate/20260109082443_add_missing_fields_to_works.rb
+++
@@ -1,7 +0,0 @@
-class AddMissingFieldsToWorks < ActiveRecord::Migration[7.1]
-  def change
-    add_column :works, :category, :string
-    add_column :works, :featured, :boolean
-    add_column :works, :position, :integer
-  end
-end
