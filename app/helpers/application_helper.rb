c661c9fccc5a9bed0fc16384ded4181606c17bff
Mathieu Heynen <contact.heynen@gmail.com>
2026-01-26 14:34:07 +0100

    Fix Cloudinary helper configuration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def cloudinary_image(image_name, options = {})
-    cl_image_tag("portfolio/#{image_name}", options)
+    # Utiliser l'URL complète sans transformation automatique
+    cl_image_tag("portfolio/#{image_name}",
+                  options.merge(
+                    secure: true,
+                    sign_url: false,
+                    type: "upload"
+                  ))
   end
 end
