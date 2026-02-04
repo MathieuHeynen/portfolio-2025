0262fd08ce7a04b7c02e6d8aedefadffe1d39788
Mathieu Heynen <contact.heynen@gmail.com>
2026-02-04 12:26:43 +0100

    updating locally and sketches work with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,31 @@ Work.create!(
   client: "Personal Work",
   title: "Character Design & Personal Illustrations",
   description: "Collection of personal artwork showcasing character design, digital illustrations, and creative explorations. Developed through years of artistic practice, these pieces demonstrate my visual storytelling abilities and unique artistic style. Created using various digital tools including Procreate, Adobe Illustrator, and Photoshop.",
-  slug: "drawings",
+  slug: "sketches",
   category: "illustration",
   featured: true,
   position: 1,
   technologies: "Procreate, Adobe Illustrator, Photoshop, Digital Drawing"
+)
+sketches.images.attach(
+  [
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203588/PORTFOLIO/DRAWINGS/SKETCHES_aw0two.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203589/PORTFOLIO/DRAWINGS/SKETCHES3_u7offg.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203589/PORTFOLIO/DRAWINGS/SKETCHES5_jwb0jf.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203589/PORTFOLIO/DRAWINGS/SKETCHES4_gn4ont.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203590/PORTFOLIO/DRAWINGS/SKETCHES6_ktjbbk.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203590/PORTFOLIO/DRAWINGS/SKETCHES2_qwbzvs.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203590/PORTFOLIO/DRAWINGS/SKETCHES7_bvxygz.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203591/PORTFOLIO/DRAWINGS/SKETCHES8_tj0l6a.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203591/PORTFOLIO/DRAWINGS/SKETCHES9_f3ac3g.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203591/PORTFOLIO/DRAWINGS/SKETCHES10_azh9fh.jpg",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770203592/PORTFOLIO/DRAWINGS/SKETCHES11_g3qgy2.jpg"
+  ].each_with_index do |url, index|
+    sketches.images.attach(
+      io: URI.open(url),
+      filename: "sketches-slide-#{index + 1}.png"
+    )
+  end
 )
 
 Work.create!(
@@ -24,6 +44,22 @@ Work.create!(
   featured: true,
   position: 2,
   technologies: "Ruby on Rails 7, PostgreSQL, Stimulus.js, Bootstrap 5, Mapbox API, Cloudinary, Figma, UI/UX Design"
+)
+locally.images.attach(
+  [
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1770200307/portfolio/locally/slide-1.png",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1764274766/PORTFOLIO/LOCALLY/LOCALLY-PRESA-05-251027_rjs4g4.png",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1764274766/PORTFOLIO/LOCALLY/LOCALLY-PRESA-02-251027_sfxhp3.png",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1764274766/PORTFOLIO/LOCALLY/LOCALLY-PRESA-04-251027_pq9gxg.png",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1764274766/PORTFOLIO/LOCALLY/LOCALLY-PRESA-03-251027_pvobnq.png",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1764274765/PORTFOLIO/LOCALLY/LOCALLY-PRESA-06-251027_ps90c5.png",
+    "https://res.cloudinary.com/dngknj8ze/image/upload/v1764274765/PORTFOLIO/LOCALLY/LOCALLY-PRESA-07-251027_vxdjmu.png"
+  ].each_with_index do |url, index|
+    locally.images.attach(
+      io: URI.open(url),
+      filename: "locally-slide-#{index + 1}.png"
+    )
+  end
 )
 
 Work.create!(
