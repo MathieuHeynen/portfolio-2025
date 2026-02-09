2c3e75871ca5b31aca1d12d519e8cd30baebd3e8
Mathieu Heynen <contact.heynen@gmail.com>
2026-02-09 13:16:03 +0100

    helper modifications, works view page and show page modifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # Méthode existante pour les images statiques
   def cloudinary_image(image_name, options = {})
     # Utiliser l'URL complète sans transformation automatique
     cl_image_tag("portfolio/#{image_name}",
@@ -7,5 +8,17 @@ module ApplicationHelper
                     sign_url: false,
                     type: "upload"
                   ))
+  end
+
+  # Nouvelle méthode pour les images ActiveStorage
+  def cloudinary_image_url(attachment)
+    return nil unless attachment&.attached?
+
+    blob = attachment.blob
+    cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+
+    # Construire l'URL Cloudinary directement depuis la clé du blob
+    # La clé contient déjà le chemin complet dans Cloudinary
+    "https://res.cloudinary.com/#{cloud_name}/image/upload/#{blob.key}"
   end
 end
